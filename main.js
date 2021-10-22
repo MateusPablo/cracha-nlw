b1c64893c778ace1add1696eb383ff08b02c4f32
Mateus Amorim <79024971+MateusPablo@users.noreply.github.com>
2021-10-21 23:16:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'mateuspablo',
+  youtube: 'channel/UCLL1g9Urd8C0v8DgZRWnk8w',
+  facebook: '100003963474521',
+  instagram: 'mateuspablo_',
+  twitter: 'mateuspablo3'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
